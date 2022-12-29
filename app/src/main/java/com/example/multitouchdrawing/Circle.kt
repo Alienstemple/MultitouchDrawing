8377ce5956ac6981e11526869c3b750968b7ea59
Alyona <borisovsk_a@mail.ru>
2022-12-29 10:31:58 +0300

    debug: Debug multitouch

--- app/src/main/java/com/example/multitouchdrawing/Circle.kt
+++ app/src/main/java/com/example/multitouchdrawing/Circle.kt
@@ -2,4 +2,4 @@ package com.example.multitouchdrawing
 
 import android.graphics.PointF
 
-data class Circle(val origin: PointF, val radius: Float)
+data class Circle(var origin: PointF, val radius: Float)
--- app/src/main/java/com/example/multitouchdrawing/MultiTouchView.kt
+++ app/src/main/java/com/example/multitouchdrawing/MultiTouchView.kt
@@ -2,42 +2,68 @@ package com.example.multitouchdrawing
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.PointF
 import android.util.AttributeSet
+import android.util.Log
+import android.util.SparseArray
 import android.view.MotionEvent
 import android.view.View
 
-class MultiTouchView(context: Context, attrs: AttributeSet?, defStyleAttrs: Int, defStyleRes: Int) :
-    View(context, attrs, defStyleAttrs, defStyleRes) {
+class MultiTouchView(context: Context, attrs: AttributeSet? = null) :
+    View(context, attrs) {
 
-    private val mCircles: ArrayList<Circle> = arrayListOf()
-    private val mPaint = Paint()
+    private val mCircles: HashMap<Int, Circle> = HashMap()
+    private val mPaint = Paint(Color.BLACK)
+    private val circleRadius = 100f
 
     init {
         setupPaint()
     }
 
     override fun onDraw(canvas: Canvas?) {
-        for (circle in mCircles) {
-            canvas?.drawCircle(circle.origin.x, circle.origin.y, circle.radius, mPaint)
+        for (i in mCircles.keys) {
+            val curCircle = mCircles[i]
+            canvas?.drawCircle(curCircle!!.origin.x, curCircle.origin.y, curCircle.radius, mPaint)  // TODO wrap to let
         }
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val action = event.actionMasked
+        val pointerIndex = event.actionIndex
+        val pointerId = event.getPointerId(pointerIndex)
+        val point = PointF(event.x, event.y)
+
 
         var currentCircle: Circle
 
         return when(action) {
             MotionEvent.ACTION_DOWN,
             MotionEvent.ACTION_POINTER_DOWN -> {
-                val point = PointF(event.x, event.y)
+                currentCircle = Circle(point, circleRadius)
+                mCircles[pointerId] = currentCircle
+                Log.d("MultLog", "Draw circle, index $pointerIndex, id $pointerId, x = ${point.x}, y = ${point.y}")
                 true
             }
-            MotionEvent.ACTION_MOVE -> true
-            MotionEvent.ACTION_POINTER_UP -> true
-            MotionEvent.ACTION_UP -> true
+            MotionEvent.ACTION_MOVE -> {
+//                for (i in 0..event.pointerCount) {
+//                    val id = event.getPointerId(i)
+//                    currentCircle = mCircles[id]!!
+//                    currentCircle.origin = PointF(event.x, event.y)
+//                }
+                invalidate()
+                true
+            }
+            MotionEvent.ACTION_POINTER_UP -> {
+                mCircles.remove(pointerId)
+                true
+            }
+            MotionEvent.ACTION_UP,
+            MotionEvent.ACTION_CANCEL -> {
+                mCircles.clear()
+                true
+            }
             else -> {super.onTouchEvent(event)}
         }
     }
